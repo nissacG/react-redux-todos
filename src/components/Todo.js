412d52239d184c0c623f23cab9bd5e0987ecc96f
Gareth Cassin <nissacjg@gmail.com>
2019-02-10 16:44:01 +0000

    move delete function up to parent, update styling and conditionally show delete button

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -2,15 +2,18 @@ import React, { Component } from 'react'
 
 class Todo extends Component {
   
-  handleDelete = (e) =>{
-    console.log('poop')
-  }
-
   render() {
     return (
-      <li>
-        {this.props.task}
-        <button onClick={this.handleDelete}>X</button>
+      <li className='card card-body text-center d-flex flex-row justify-content-around align-items-center mb-2' style={{ padding: '0.7rem' }}>
+        <span className='col-md-9'>{this.props.task}</span>
+        {this.props.delete &&
+          <button
+            className='btn btn-outline-secondary'
+            onClick={() => this.props.delete(this.props.id)}
+          >
+            X
+          </button>
+        }
       </li>
     )
   }
